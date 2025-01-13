0977d0f166f858def059b3e01004e1af32612b8a
Ludvik Jerabek <ljerabek@proofpoint.com>
2025-01-12 22:13:11 -0500

    Update evidence_types.py

--- src/tap_api/v2/endpoints/forensics/evidence_types.py
+++ src/tap_api/v2/endpoints/forensics/evidence_types.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import Dict, Type, Optional
+from typing import Dict, Type, Optional, List
+
 
 class EvidenceType(Dict):
     """
@@ -22,7 +23,7 @@ class AttachmentEvidenceType(EvidenceType):
         return self.get("sha256", "")
 
     @property
-    def blacklisted(self) -> int:
+    def blacklisted(self) -> bool:
         return self.get("blacklisted", False)
 
     @property
@@ -70,6 +71,41 @@ class CookieEvidenceType(EvidenceType):
     def value(self) -> str:
         return self.get("value", "")
 
+class DNSEvidenceType(EvidenceType):
+    @property
+    def host(self) -> str:
+        return self.get("host", "")
+
+    @property
+    def cnames(self) -> List[str]:
+        return self.get("cnames", [])
+
+    @property
+    def ips(self) -> List[str]:
+        return self.get("ips", [])
+
+    @property
+    def nameservers(self) -> List[str]:
+        return self.get("nameservers", [])
+
+    @property
+    def nameservers_list(self) -> List[str]:
+        return self.get("nameserversList", [])
+
+
+class DropperEvidenceType(EvidenceType):
+    @property
+    def path(self) -> str:
+        return self.get("path", "")
+
+    @property
+    def rule(self) -> str:
+        return self.get("rule", "")
+
+    @property
+    def url(self) -> str:
+        return self.get("url", "")
+
 class FileEvidenceType(EvidenceType):
     @property
     def sha256(self) -> str:
@@ -83,10 +119,15 @@ class FileEvidenceType(EvidenceType):
     def path(self) -> str:
         return self.get("path", "")
 
-class URLEvidenceType(EvidenceType):
+class IDSEvidenceType(EvidenceType):
     @property
-    def url(self) -> str:
-        return self.get("url", "")
+    def name(self) -> str:
+        return self.get("name", "")
+
+    @property
+    def signature_id(self) -> int:
+        return self.get("signatureId", 0)
+
 
 class MutexEvidenceType(EvidenceType):
     def __init__(self, data: Dict):
@@ -101,8 +142,7 @@ class MutexEvidenceType(EvidenceType):
     def path(self) -> str:
         return self.get("path", "")
 
-
-class ActionEvidenceType(EvidenceType):
+class NetworkEvidenceType(EvidenceType):
     @property
     def action(self) -> str:
         return self.get("action", "")
@@ -112,22 +152,106 @@ class ActionEvidenceType(EvidenceType):
         return self.get("ip", "")
 
     @property
-    def port(self) -> int:
-        return self.get("port", 0)
+    def port(self) -> str:
+        return self.get("port", "")
+
+    @property
+    def protocol_type(self) -> str:
+        return self.get("type", "")
+
+class ProcessEvidenceType(EvidenceType):
+    @property
+    def path(self) -> str:
+        return self.get("path", "")
+
+    @property
+    def action(self) -> str:
+        return self.get("action", "")
+
+class RegistryEvidenceType(EvidenceType):
+    @property
+    def action(self) -> str:
+        return self.get("action", "")
+
+    @property
+    def key(self) -> str:
+        return self.get("key", "")
+
+    @property
+    def name(self) -> str:
+        return self.get("name", "")
+
+    @property
+    def rule(self) -> str:
+        return self.get("rule", "")
+
+    @property
+    def value(self) -> str:
+        return self.get("value", "")
+
+
+class ScreenshotEvidenceType(EvidenceType):
+    @property
+    def url(self) -> str:
+        return self.get("url", "")
+
+
+class URLEvidenceType(EvidenceType):
+    @property
+    def url(self) -> str:
+        return self.get("url", "")
+
+    @property
+    def blacklisted(self) -> bool:
+        return self.get("blacklisted", False)
+
+    @property
+    def ip(self) -> str:
+        return self.get("ip", "")
+
+    @property
+    def http_status(self) -> int:
+        return self.get("httpStatus", 0)
+
+    @property
+    def md5(self) -> str:
+        return self.get("md5", "")
+
+    @property
+    def offset(self) -> int:
+        return self.get("offset", 0)
+
+    @property
+    def rule(self) -> str:
+        return self.get("rule", "")
+
+    @property
+    def sha256(self) -> str:
+        return self.get("sha256", "")
+
+    @property
+    def size(self) -> int:
+        return self.get("size", 0)
 
 
 def create_evidence_type(data: Dict, evidence_type: str) -> EvidenceType:
-    subclass = _WHAT_INFO_REGISTRY.get(evidence_type, EvidenceType)
+    subclass = _EVIDENCE_TYPE_REGISTRY.get(evidence_type, EvidenceType)
     return subclass(data)
 
 # Registry for WhatInfo subclasses
-_WHAT_INFO_REGISTRY: Dict[str, Type[EvidenceType]] = {
+_EVIDENCE_TYPE_REGISTRY: Dict[str, Type[EvidenceType]] = {
     "attachment": AttachmentEvidenceType,
     "behavior": BehaviorEvidenceType,
     "cookie": CookieEvidenceType,
+    "dns": DNSEvidenceType,
+    "dropper": DropperEvidenceType,
     "rule": RuleEvidenceType,
     "file": FileEvidenceType,
-    "url": URLEvidenceType,
+    "ids": IDSEvidenceType,
     "mutex": MutexEvidenceType,
-    "action": ActionEvidenceType,
+    "network": NetworkEvidenceType,
+    "process": ProcessEvidenceType,
+    "registry": RegistryEvidenceType,
+    "screenshot": ScreenshotEvidenceType,
+    "url": URLEvidenceType,
 }
